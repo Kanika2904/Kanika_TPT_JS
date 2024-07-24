5cb79ebab1cf0ca64e472b950be542872c1ca859
Kanika Agarwal <kanikaagarwal2909@gmail.com>
2024-07-24 22:19:56 +0530

    Question 2 Done!

---
+++ JS2.js
@@ -0,0 +1,18 @@
+let x = 50;
+let y = 10;
+let max;
+
+if (x > y) {
+  max = x;
+} else {
+  max = y;
+}
+
+console.log("The larger value is: " + max); 
+
+let a = 5;
+let b = 10;
+
+let max1 = (a > b) ? a : b;
+
+console.log("The larger value is: " + max1);  \
